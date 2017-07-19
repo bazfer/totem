a4f5f5282b8f728ff895ef6c5e5123fa2e84e2f2
Fernando Baz <Fernando@BazNakauma-Macbook-Pro-2.local>
2017-07-19 09:26:31 -0500

    test

--- client/src/modules/totem/components/totem.js
+++ client/src/modules/totem/components/totem.js
@@ -2,7 +2,9 @@ import React from 'react'
 import TotemController from '../controllers/TotemController'
 
 const Totem = () => (
+  <div className='totem'>
     <TotemController />
+  </div>
 )
 
 export default Totem
