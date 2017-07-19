1eeff71ca3ea40019b7c46aee69209a868b29857
Fernando Baz <Fernando@BazNakauma-Macbook-Pro-2.local>
2017-07-19 09:33:12 -0500

    no refactor necessary now, quick fix

--- client/src/modules/totem/components/totem.js
+++ client/src/modules/totem/components/totem.js
@@ -2,9 +2,7 @@ import React from 'react'
 import TotemController from '../controllers/TotemController'
 
 const Totem = () => (
-  <div className='totem'>
     <TotemController />
-  </div>
 )
 
 export default Totem
